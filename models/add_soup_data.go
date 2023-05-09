77696f5843b316eafc8085ff19706972c3432dd2
joe <chries-rong@protonmail.com>
2023-05-09 09:05:54 +0800

    fix bug

--- models/add_soup_data.go
+++ models/add_soup_data.go
@@ -19,17 +19,17 @@ type AddSoupData struct {
 }
 
 type AddSoupDataValue struct {
-	Id               string `gorm:"column:id;NOT NULL" json:"id,omitempty"`
-	ShopName         string `gorm:"column:name;NOT NULL"`
-	OrderSn          string `gorm:"column:order_sn;NOT NULL"`
-	BottomPot        string `gorm:"column:bottom_pot;NOT NULL"`
-	TableNumber      string `gorm:"column:table_number"`
-	OrderTime        int64  `gorm:"column:order_time"`
-	SoupStartTime    int64  `gorm:"column:soup_start_time"`
-	SoupEndTime      int64  `gorm:"column:soup_end_time"`
-	FeedingStartTime int64  `gorm:"column:feeding_start_time"`
-	FeedingEndTime   int64  `gorm:"column:feeding_end_time"`
-	TurningPotEnd    int64  `gorm:"column:turning_pot_end_time"`
+	Id               string    `gorm:"column:id;NOT NULL" json:"id,omitempty"`
+	ShopName         string    `gorm:"column:name;NOT NULL"`
+	OrderSn          string    `gorm:"column:order_sn;NOT NULL"`
+	BottomPot        string    `gorm:"column:bottom_pot;NOT NULL"`
+	TableNumber      string    `gorm:"column:table_number"`
+	OrderTime        time.Time `gorm:"column:order_time"`
+	SoupStartTime    time.Time `gorm:"column:soup_start_time"`
+	SoupEndTime      time.Time `gorm:"column:soup_end_time"`
+	FeedingStartTime time.Time `gorm:"column:feeding_start_time"`
+	FeedingEndTime   time.Time `gorm:"column:feeding_end_time"`
+	TurningPotEnd    time.Time `gorm:"column:turning_pot_end_time"`
 }
 
 func (a *AddSoupData) TableName() string {
